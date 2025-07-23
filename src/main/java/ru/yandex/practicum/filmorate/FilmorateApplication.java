e084737efd80c0a78e1cf59beee9308ed3d61506
Vandal <arhvandal@yandex.ru>
2025-07-24 00:02:16 +0300

    Check style

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Bean;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
-import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
-import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 @SpringBootApplication
 public class FilmorateApplication {
