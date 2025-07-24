eadbd1426951949014406ab7a81fdb1bff54f100
Vandal <arhvandal@yandex.ru>
2025-07-24 16:55:00 +0300

    Check style

--- src/test/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorageTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
